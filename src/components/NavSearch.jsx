42eed045107f4bd2fe864b96ce30f800aff20c35
Odoikenna1 <davidodojunior@gmail.com>
2024-12-03 16:30:16 +0100

    search parameters edited to match request object

--- src/components/NavSearch.jsx
+++ src/components/NavSearch.jsx
@@ -5,27 +5,27 @@ const NavSearch = () =>{
             <div className='navArea'>
                 <div className='input-section'>
                     <div className='input-header'>
-                        <h5>INDUSTRY</h5>
-                    </div>
-                    <div><input type='text' placeholder='Enter industry'/></div>
-                </div>
-                <div className='input-section'>
-                    <div className='input-header'>
                         <h5>OCCUPATION</h5>
                     </div>
                     <div><input type='text' placeholder='Enter occupation'/></div>
                 </div>
                 <div className='input-section'>
                     <div className='input-header'>
-                        <h5>LOCATION</h5>
+                        <h5>CITY</h5>
                     </div>
-                    <div><input type='text' placeholder='Enter location'/></div>
+                    <div><input type='text' placeholder='Enter city'/></div>
                 </div>
                 <div className='input-section'>
                     <div className='input-header'>
                         <h5>KEYWORD</h5>
                     </div>
                     <div><input type='text' placeholder='Enter keyword'/></div>
+                </div>
+                <div className='input-section'>
+                    <div className='input-header'>
+                        <h5>FILETYPE</h5>
+                    </div>
+                    <div><input type='text' placeholder='Enter file type'/></div>
                 </div>
                 <button>
                     Find my leads
